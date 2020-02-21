fd60bd77b63e8b67fbc102e976973638aab86f01
pugege2018 <37761975+pugege2018@users.noreply.github.com>
2020-02-21 21:01:05 +0800

    剑值offer 02 替换空格 replace方法 String 和 Stringbuffer StringBuilder区别

--- src/com/tools/Print.java
+++ src/com/tools/Print.java
@@ -3,7 +3,34 @@ package com.tools;
 import com.pu.链表.Node;
 import com.tools.Print;
 
+import java.util.List;
+
 public class Print {
+
+    public static<T> void listListPrint(List<List<T>> list){
+        if(list == null || list.size() == 0){
+            System.out.println("{}");
+            return;
+        }
+        for(int i = 0; i < list.size(); i++){
+            if(i == 0){
+                System.out.print("{");
+            }
+            for(int j = 0; j < list.get(i).size(); j++){
+                if(j == 0){
+                    System.out.print("[" + list.get(i).get(j));
+                }else if(j == list.get(i).size() - 1){
+                    System.out.print("," + list.get(i).get(j) + "]");
+                }else {
+                    System.out.print("," + list.get(i).get(j));
+                }
+            }
+            if(i == list.size() - 1){
+                System.out.print("}");
+            }
+        }
+        System.out.println();
+    }
     public static void printArray(Integer[] arr){
         if(arr.length == 0){
             System.out.println("This array is empty");
