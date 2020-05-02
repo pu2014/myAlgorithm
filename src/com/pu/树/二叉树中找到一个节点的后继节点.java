3d25d9d4c6732949cc9660abaae62290e38c01e8
pu2014 <whutpbj@163.com>
2020-05-02 14:09:58 +0800

    二叉树后继节点

--- src/com/pu/树/二叉树中找到一个节点的后继节点.java
+++ src/com/pu/树/二叉树中找到一个节点的后继节点.java
@@ -39,29 +39,62 @@ public class 二叉树中找到一个节点的后继节点 {
         node6.parent = node4;
 
         Node res1 = getSuccessorNode(head);//66
-        System.out.println(res1.value);
+        System.out.println(res1.value);//68
         Node res2 = getSuccessorNode(node4);//73
-        System.out.println(res2.value);
+        System.out.println(res2.value);//89
         Node res3 = getSuccessorNode(node2);//35
-        System.out.println(res3.value);
+        System.out.println(res3.value);//45
         Node res4 = getSuccessorNode(node3);//56
-        System.out.println(res4.value);
+        System.out.println(res4.value);//66
+
+        System.out.println(getSuccessorNodeByNoParent(head, node4).value);//89
     }
 
-    public static Node getSuccessorNode(Node node){
+    /**
+     * 有父节点，如果当前节点有右节点，找右节点的最左节点就可
+     *      如果当前节点没有右节点，找当前节点的父节点，
+     *                  当节点为父节点左节点，说明已经遍历过，再继续找
+     *                  如果当前节点为父节点的左节点，返回该节点
+     * @param node
+     * @return
+     */
+    public static Node getSuccessorNode(Node node) {
         if (node.right != null) {
             node = node.right;
-            while(node.left != null){
+            while (node.left != null) {
                 node = node.left;
             }
             return node;
-        }else{
+        } else {
             Node parent = node.parent;
-            while(parent != null && parent.left != node){
+            while (parent != null && parent.left != node) {
                 node = parent;
                 parent = node.parent;
             }
-                return parent;
+            return parent;
         }
+    }
+
+    /**
+     * 没有父节点，思路
+     *      如果cur.val 比 root.val 值大，肯定在右节点上，从右节点找，找不到直接为null
+     *      反之，在左节点上找。找不到就是root
+     *
+     * @param root
+     * @param node
+     * @return
+     */
+    public static Node getSuccessorNodeByNoParent(Node root, Node node){
+        if(root == null || node == null){
+            return null;
+        }
+        if(root.value <= node.value){
+            Node right = getSuccessorNodeByNoParent(root.right, node);
+            return right;
+        }else{
+            Node left = getSuccessorNodeByNoParent(root.left, node);
+            return left == null ? root : node;
+        }
+
     }
 }
