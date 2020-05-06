5327ed39cc34da19893d964c656cd2ac035221fa
pu2014 <whutpbj@163.com>
2020-05-06 12:24:31 +0800

    572 另一个树的子树的优化版本

--- src/com/pu/树/_572另一个树的子树.java
+++ src/com/pu/树/_572另一个树的子树.java
@@ -71,4 +71,20 @@ class IsSubtree {
         }
         return false;
     }
+
+    // 优化后一步到位
+    public boolean checkSubTree(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null){
+            return true;
+        }
+        boolean res = false;
+        if(t1 != null && t2 != null){
+            if(t1.val == t2.val){
+                res = checkSubTree(t1.left, t2.left) && checkSubTree(t1.right, t2.right);
+            }else{
+                res = res || checkSubTree(t1.left, t2) || checkSubTree(t1.right, t2);
+            }
+        }
+        return res;
+    }
 }
