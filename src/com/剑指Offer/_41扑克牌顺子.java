ee1d83ed66438a354395e9208d3d9b6ab1b48c8d
pu2014 <whutpbj@163.com>
2020-05-19 20:10:06 +0800

    41 扑克牌顺子

---
+++ src/com/剑指Offer/_41扑克牌顺子.java
@@ -0,0 +1,54 @@
+package com.剑指Offer;
+
+import java.util.Scanner;
+
+/**
+ * @DESC LL今天心情特别好,因为他去买了一副扑克牌,发现里面居然有2个大王,2个小王(一副牌原本是54张^_^)...他
+ * 随机从中抽出了5张牌,想测测自己的手气,看看能不能抽到顺子,如果抽到的话,他决定去买体育彩票,嘿嘿！！“红心A,
+ * 黑桃3,小王,大王,方片5”,“Oh My God!”不是顺子.....LL不高兴了,他想了想,决定大\小 王可以看成任何数字,并且
+ * A看作1,J为11,Q为12,K为13。上面的5张牌就可以变成“1,2,3,4,5”(大小王分别看作2和4),“So Lucky!”。LL决定
+ * 去买体育彩票啦。 现在,要求你使用这幅牌模拟上面的过程,然后告诉我们LL的运气如何，
+ * 如果牌能组成顺子就输出true，否则就输出false。为了方便起见,你可以认为大小王是0。
+ * @CREATE BY @Author pbj on @Date 2020/5/19 19:59
+ */
+public class _41扑克牌顺子 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] nums = new int[N];
+        for(int i = 0; i < N; i++){
+            nums[i] = sc.nextInt();
+        }
+        IsContinuous proc = new IsContinuous();
+        System.out.println(proc.fun(nums));
+    }
+}
+class IsContinuous {
+    public boolean fun(int [] nums) {
+        if(nums == null || nums.length == 0){
+            return false;
+        }
+        int[] poker = new int[14];
+        int min = 13;
+        int max = -1;
+        for(int num : nums){
+            if(num == 0){
+                poker[num]++;
+            }else{
+                if(poker[num] != 0){
+                    return false;
+                }else{
+                    if(num < min){
+                        min = num;
+                    }
+                    if(num > max){
+                        max = num;
+                    }
+                    poker[num]++;
+                }
+            }
+
+        }
+        return max - min < 5;
+    }
+}
