11a6d49ac8deeb5d268a49da7a36aff2cccc468d
pu2014 <whutpbj@163.com>
2020-06-04 21:00:44 +0800

    _35两个链表的第一个公共结点

--- src/com/剑指Offer/_35两个链表的第一个公共结点.java
+++ src/com/剑指Offer/_35两个链表的第一个公共结点.java
@@ -25,22 +25,11 @@ class FindFirstCommonNode {
         }
         ListNode p1 = pHead1;
         ListNode p2 = pHead2;
-        while(p1 != null && p2 != null){
-            if(p1.val == p2.val && p1.next == p2.next){
-                return p1;
-            }
-            p1 = p1.next;
-            p2 = p2.next;
-            if(p1 != p2){
-                if(p1 == null){
-                    p1 = pHead2;
-                }
-                if(p2 == null){
-                    p2 = pHead1;
-                }
-            }
+        while(p1 != p2){
+            p1 = p1 == null ? pHead2 : p1.next;
+            p2 = p2 == null ? pHead1 : p2.next;
         }
-        return null;
+        return p1;
     }
 }
 
