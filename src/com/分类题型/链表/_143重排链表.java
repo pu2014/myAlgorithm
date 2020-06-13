5721c138d3a06f9a2e6ae2e1420150b6f0b82a2f
pu2014 <whutpbj@163.com>
2020-06-13 23:53:50 +0800

    _143重排链表

---
+++ src/com/分类题型/链表/_143重排链表.java
@@ -0,0 +1,62 @@
+package com.分类题型.链表;
+
+import com.tools.ListNode;
+
+/**
+ * @DESC 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+ * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ * 示例 1:
+ *
+ * 给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+ * 示例 2:
+ *
+ * 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reorder-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @CREATE BY @Author pbj on @Date 2020/6/13 23:51
+ */
+public class _143重排链表 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+    }
+}
+class ReorderList {
+    public void reorder(ListNode head) {
+        if(head == null || head.next == null){
+            return;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode newHead = reverseList(slow.next);
+        slow.next = null;
+        while(newHead != null){
+            ListNode next = newHead.next;
+            newHead.next = head.next;
+            head.next = newHead;
+            head = newHead.next;
+            newHead = next;
+        }
+    }
+    public ListNode reverseList(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
