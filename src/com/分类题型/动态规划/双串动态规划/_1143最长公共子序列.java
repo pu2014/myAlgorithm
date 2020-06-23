52f083d3b99e3ef3c52d05e71466a757044c34eb
pu2014 <whutpbj@163.com>
2020-06-23 11:36:44 +0800

    _1143最长公共子序列

--- src/com/分类题型/动态规划/双串动态规划/_1143最长公共子序列.java
+++ src/com/分类题型/动态规划/双串动态规划/_1143最长公共子序列.java
@@ -1,5 +1,7 @@
 package com.分类题型.动态规划.双串动态规划;
 
+import java.util.Scanner;
+
 /**
  * Description:给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
  * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
@@ -11,4 +13,26 @@ package com.分类题型.动态规划.双串动态规划;
  * Created By @Author my on @Date 2020/6/3 10:45
  */
 public class _1143最长公共子序列 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text1 = sc.nextLine();
+        String text2 = sc.nextLine();
+        System.out.println(new Solution().longestCommonSubsequence(text1, text2));
+
+    }
+    static class Solution{
+        public int longestCommonSubsequence(String text1, String text2) {
+            int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+            for(int i = 1; i <= text1.length(); i++){
+                for(int j = 1; j <= text2.length(); j++){
+                    if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    }else{
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+            return dp[text1.length()][text2.length()];
+        }
+    }
 }
