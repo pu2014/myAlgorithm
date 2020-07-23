b06d719bb81e9ac863c756afb160a75efe73617b
pu2014 <whutpbj@163.com>
2020-07-23 12:52:41 +0800

    1. LinkedHashMap设计LRU缓存，主要参数 访问参数设置为true，插入顺序是 false

---
+++ src/com/分类题型/高级算法/LRUByJava.java
@@ -0,0 +1,22 @@
+package com.分类题型.高级算法;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @DESC
+ * @CREATE BY @Author pbj on @Date 2020/7/23 12:47
+ */
+public class LRUByJava extends LinkedHashMap<Integer, Integer> {
+    private int capacity;
+
+    public LRUByJava(int capacity){
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+    public int get(int key){
+        return super.getOrDefault(key, -1);
+    }
+    public void put(int key, int value){
+        super.put(key, value);
+    }
+}
